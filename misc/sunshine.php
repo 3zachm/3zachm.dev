c0f4a9d02e287a89ed3265b79cf2a62d24f91355
3zachm <3zachm2@gmail.com>
2021-05-13 01:12:51 -0700

    fixed basic logic error

--- misc/sunshine.php
+++ misc/sunshine.php
@@ -3,13 +3,13 @@
     $minute = date('i');
     $ampm = date('a');
     $var = "cannot";
-    if ($apm === "am") {
-        if ($hour > 5) {
+    if ($ampm === "am") {
+        if ($hour > 5 && $hour < 12) {    // if greater than 5, and less than 12
             $var = "can";
         }
     }
     else {
-        if ($hour < 6) {
+        if ($hour < 6 || $hour > 11) {   // if less than 6 or greater than 11
             $var = "can";
         }
     }
