433d500fa89587099f6d41afbc484f02477c48da
3zachm <3zachm2@gmail.com>
2021-05-13 15:19:02 -0700

    don't code past midnight

--- misc/sunshine.php
+++ misc/sunshine.php
@@ -1,17 +1,4 @@
 <?php
-    $hour = date('h');
-    $minute = date('i');
-    $ampm = date('a');
-    $var = "cannot";
-    if ($ampm === "am") {
-        if ($hour > 5 && $hour < 12) {    // if greater than 5, and less than 12
-            $var = "can";
-        }
-    }
-    else {
-        if ($hour < 6 || $hour > 11) {   // if less than 6 or greater than 11
-            $var = "can";
-        }
-    }
-    echo("it is $hour:$minute $ampm, I $var, in fact, feel the sunshine");
+    $var = date('H') < 18 && date('H') > 5 ? 'can' : 'cannot';
+    echo('it is ' . date('h') . ':' . date('i') . date('a') . ', I '. $var . ', in fact, feel the sunshine');
 ?>
