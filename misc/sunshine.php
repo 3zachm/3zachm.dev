5efa0c61ddde9415f070e94fa3d65d31f359e652
3zachm <3zachm2@gmail.com>
2021-07-07 13:34:34 -0700

    sunshine fix

--- misc/sunshine.php
+++ misc/sunshine.php
@@ -1,4 +1,4 @@
 <?php
-    $var = date('H') < 18 && date('H') > 5 ? 'can' : 'cannot';
+    $var = date('H') < 19 && date('H') > 5 ? 'can' : 'cannot';
     echo('it is ' . date('h') . ':' . date('i') . date('a') . ', I '. $var . ', in fact, feel the sunshine');
 ?>
