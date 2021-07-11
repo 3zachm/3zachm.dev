2ea086755fc4c10711717dfd6815166aa81a5f98
3zachm <3zachm2@gmail.com>
2021-07-11 05:08:37 -0700

    error page init

---
+++ error/index.php
@@ -0,0 +1,51 @@
+<?php
+    if (isset($_GET["n"])) {
+        $error = $_GET["n"];
+    }
+    else {
+        $error = "404";
+    }
+?>
+<!DOCTYPE html>
+<head>
+    <title>404 | 3zachm.dev</title>
+    <!-- Titles -->
+    <meta name="title" content="404 | 3zachm.dev">
+    <meta property="og:title" content="404 | 3zachm.dev">
+    <meta property="twitter:title" content="404 | 3zachm.dev">
+    <!--  <meta property="twitter:image" content="https://3zachm.dev/404"> -->
+    <!-- <meta property="og:image" content="'.$META_IMG.'"> -->
+    <meta name="description" content="404. Doesn't necessarily mean nothing...">
+    <meta property="og:description" content="404. Doesn't necessarily mean nothing...">
+    <meta property="twitter:description" content="404. Doesn't necessarily mean nothing...">
+
+    <!-- OpenGraph/Discord -->
+    <meta property="og:url" content="https://3zachm.dev/404">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#b875d7" data-react-helmet="true" >
+
+    <!-- Twitter -->
+    <meta property="twitter:url" content="https://3zachm.dev/404">
+    <meta property="twitter:card" content="summary">
+
+    <link rel="stylesheet" href="css/error.css???">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Manrope">
+</head>
+<body>
+    <header class="main-header" id="main-header">
+        <div class="video">
+        </div>
+        <div class="main-menu">
+            <a href="/" target="_self" class="a-menu" id="logo">3zachm.dev</a>
+            <div class="main-menu-sep"></div>
+            <a href="/" target="_self" class="a-menu" id="home-menu">Home</a>
+            <a href="javascript:boots_init();" class="a-menu" id="anny-menu">Rain</a>
+        </div>
+        <div class="main-header-center">
+            <p id="four"><?php echo($error) ?></p>
+        </div>
+    </header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vtt.js/0.12.1/vtt.min.js" integrity="sha384-al9I19ONGTJ5nA5SlGvEeUl5youezo+vWKHI7u307OomzK/mSVtWphp6wSdSSNSD" crossorigin="anonymous"></script>
+    <script src="js/error.js???"></script>
+</body>
---
+++ error/js/error.js
@@ -0,0 +1,109 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var boots_vid = null;
+var cleanup = false;
+var set_boots_state = false;
+
+async function boots_init() {
+    await boots();
+}
+
+async function boots_cleanup() {
+    await sleep(1000);
+    boots_vid = null;
+    cleanup = false;
+    $('#four').html("inabakumori - Rainy Boots");
+    $('div.video').fadeIn(400);
+    return;
+}
+
+async function boots() {
+    if (boots_vid !== null) { return; }
+
+    $("#404").fadeOut(1000);
+    $("div.video").fadeOut(1000);
+    await sleep(1000)
+    $("#four").html("");
+    $("div.video").fadeIn(500);
+    $("#404").fadeIn(1000);
+
+    $("div.video").html('<video autoplay id="boots-video"><source src="../../video/boots.webm" volume=0.3 controls="false" type="video/webm"><track label="default" kind="subtitles" srclang="en" src="../../video/boots.vtt" default></video>');
+    //$("#four").css('background', 'rgba(31, 31, 31, 0.6)')
+    $("#four").css('font-size', '48px')
+    $("#four").css('color', 'black')
+    $("#four").css('font-weight', 'bold')
+    $("#four").css('text-shadow', '-1px -1px 0 #b8b4b4, 0 -1px 0 #b8b4b4, 1px -1px 0 #b8b4b4, 1px 0 0 #b8b4b4, 1px 1px 0 #b8b4b4, 0 1px 0 #b8b4b4, -1px 1px 0 #b8b4b4, -1px  0 0 #b8b4b4')
+    var a_sheet = document.createElement('style')
+    a_sheet.innerHTML = ".a-menu, .main-menu-sep, #logo, #home-menu, #anny-menu {color: black;} .main-menu-sep {border-left: 1px solid rgb(0, 0, 0)}";
+    document.body.appendChild(a_sheet);
+
+    var video = document.querySelector('#boots-video')
+    tracks = video.textTracks[0],
+        tracks.mode = 'hidden', // must occur before cues is retrieved
+        cues = tracks.cues;
+
+    video.volume = 0.3;
+
+    var replaceText = function (text) {
+        $('#four').html(text);
+    },
+
+        showText = function () {
+            $('#four').show();
+        },
+
+        hideText = function () {
+            $('#four').hide();
+        },
+
+        cueEnter = function () {
+            replaceText(this.text);
+            showText();
+        },
+
+        cueExit = function () {
+            hideText();
+        },
+
+        videoLoaded = function (e) {
+            for (var i in cues) {
+                var cue = cues[i];
+                cue.onenter = cueEnter;
+                cue.onexit = cueExit;
+            }
+        },
+
+        playVideo = function (e) {
+            video.play();
+        };
+
+
+    video.addEventListener('loadedmetadata', videoLoaded);
+    video.addEventListener('load', playVideo);
+    video.load();
+
+    video.ontimeupdate = function () { boots_time(video.currentTime) }
+
+    async function boots_time(time) {
+        console.log(time)
+        if (cleanup) { boots_cleanup(); cleanup = false; video.ontimeupdate = null; }
+        if (time > 116.5 && time < 117.5) {
+            if (!set_boots_state) {
+                console.log("yep")
+                document.body.removeChild(a_sheet);
+                set_boots_state = true;
+            }
+        }
+        else if (time > 123.5 && time < 124.5) {
+            document.body.appendChild(a_sheet);
+        }
+        else if (parseInt(time) == 191) {
+            $("div.main-header-center").fadeOut(400);
+            $('div.video').fadeOut(400);
+            document.body.removeChild(a_sheet);
+            cleanup = true;
+        }
+    }
+}
